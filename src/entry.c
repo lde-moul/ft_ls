ba4a6dcfbe304427de949cec1c510afe8b7f68bf
Louis-antoine DE MOULINS DE ROCHEFORT <lde-moul@e2r2p3.42.fr>
2017-12-18 17:10:06 +0100

    Display entry mode

--- src/entry.c
+++ src/entry.c
@@ -13,7 +13,35 @@
 #include "ft_ls.h"
 #include "libft.h"
 
+static void	display_entry_mode(mode_t mode)
+{
+	if ((mode & S_IFMT) == S_IFREG)
+		ft_putchar('-');
+	else if ((mode & S_IFMT) == S_IFDIR)
+		ft_putchar('d');
+	else if ((mode & S_IFMT) == S_IFLNK)
+		ft_putchar('l');
+	else if ((mode & S_IFMT) == S_IFSOCK)
+		ft_putchar('s');
+	else if ((mode & S_IFMT) == S_IFIFO)
+		ft_putchar('p');
+	else if ((mode & S_IFMT) == S_IFBLK)
+		ft_putchar('b');
+	else if ((mode & S_IFMT) == S_IFCHR)
+		ft_putchar('c');
+	ft_putchar(mode & S_IRUSR ? 'r' : '-');
+	ft_putchar(mode & S_IWUSR ? 'w' : '-');
+	ft_putchar(mode & S_IXUSR ? 'x' : '-');
+	ft_putchar(mode & S_IRGRP ? 'r' : '-');
+	ft_putchar(mode & S_IWGRP ? 'w' : '-');
+	ft_putchar(mode & S_IXGRP ? 'x' : '-');
+	ft_putchar(mode & S_IROTH ? 'r' : '-');
+	ft_putchar(mode & S_IWOTH ? 'w' : '-');
+	ft_putchar(mode & S_IXOTH ? 'x' : '-');
+}
+
 void	display_entry(t_entry *entry)
 {
+	display_entry_mode(entry->info.st_mode);
 	ft_putendl(entry->name);
 }
