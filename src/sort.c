aeee2c20a9a86c22db375b4b5304c03b7563ff8e
Louis-antoine DE MOULINS DE ROCHEFORT <lde-moul@e1r7p15.42.fr>
2018-01-10 15:13:31 +0100

    Fix sort order

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 17:09:53 by lde-moul          #+#    #+#             */
-/*   Updated: 2018/01/09 17:20:34 by lde-moul         ###   ########.fr       */
+/*   Updated: 2018/01/10 15:12:59 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 
 static int	sorted_by_name(t_entry *entry1, t_entry *entry2)
 {
-	return (ft_strcmp(entry1->name, entry2->name) < 0);
+	return (ft_strcmp(entry1->name, entry2->name) > 0);
 }
 
 static int	sorted_by_name_reverse(t_entry *entry1, t_entry *entry2)
 {
-	return (ft_strcmp(entry1->name, entry2->name) > 0);
+	return (ft_strcmp(entry1->name, entry2->name) < 0);
 }
 
 static int	sorted_by_time(t_entry *entry1, t_entry *entry2)
 {
-	return (entry1->info.st_mtimespec.tv_sec >
+	return (entry1->info.st_mtimespec.tv_sec <
 			entry2->info.st_mtimespec.tv_sec);
 }
 
 static int	sorted_by_time_reverse(t_entry *entry1, t_entry *entry2)
 {
-	return (entry1->info.st_mtimespec.tv_sec <
+	return (entry1->info.st_mtimespec.tv_sec >
 			entry2->info.st_mtimespec.tv_sec);
 }
 
